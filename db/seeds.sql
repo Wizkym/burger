4c4e413a925fabe7d6a3203e700992c47010222d
Kelvin Jones <kevinjones4453@gmail.com>
2018-11-27 23:30:01 -0500

    created files and directories

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured) 
+VALUES ('Double Bacon Cheeseburger', false),
+    ('Triple-Stacked Avocado Burger', false),
+    ('Southern Style Veggie Burger', true);
